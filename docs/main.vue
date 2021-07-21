693b9880d88cf16641faacbe5e9c089b988794f7
LJ <2180727350@qq.com>
2021-07-21 15:56:43 +0800

    fix: 修复docs 按钮在mac 上不居中的问题

--- docs/main.vue
+++ docs/main.vue
@@ -17,11 +17,9 @@ export default class Main extends Vue {}
 
 <style lang="less" scoped>
 .fixed {
-    position: fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    margin: auto;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 }
 </style>
