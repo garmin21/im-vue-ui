6576a54b55189fa6f71c11b00cb369680a6d3ab2
LJ <2180727350@qq.com>
2021-07-31 14:09:59 +0800

    fix: 缓存依赖

--- package/input/input.vue
+++ package/input/input.vue
@@ -1,12 +1,19 @@
 <template>
     <label :class="classes">
-        <input :type="type" :value="value" @focus="handelInputFocus" @blur="handelInputBlur" @input="handelInput" :class="[`${prefixcls}__input`]" />
+        <input
+            :type="type"
+            :value="value"
+            @focus="handelInputFocus"
+            @blur="handelInputBlur"
+            @input="handelInput"
+            :class="[`${prefixcls}__input`]"
+        />
     </label>
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator';
-import { PREFIXCLS } from '../theme-chalk/var';
+import { Vue, Component, Prop } from "vue-property-decorator";
+import { PREFIXCLS } from "../theme-chalk/var";
 
 /**
  * 1. 需求点1: 希望可以限制不同类型，根据类型限制值的输入
@@ -16,59 +23,52 @@ export default class JmInput extends Vue {
     // 类型
     @Prop({
         type: String,
-        default: 'text'
+        default: "text"
     })
-    public type!:string;
-    
+    public type!: string;
+
     @Prop({
         type: String,
         required: true
     })
-    public value!:string;
+    public value!: string;
 
     public get classes() {
         const { prefixcls } = this;
-        return [
-            `${prefixcls}__input__block`
-        ]
+        return [`${prefixcls}__input__block`];
     }
 
     public get prefixcls() {
         return PREFIXCLS;
     }
 
-
-    public handelInput(evt:Event) {
+    public handelInput(evt: Event) {
         const target = evt.target as HTMLInputElement;
-        this.$emit('input', target.value);
-        this.$emit('handel-change',target.value)
+        this.$emit("input", target.value);
+        this.$emit("handel-change", target.value);
     }
 
     public handelInputFocus() {
-        this.$emit('focus')
+        this.$emit("focus");
     }
 
     public handelInputBlur() {
-        this.$emit('blur')
+        this.$emit("blur");
     }
 }
 </script>
 
 <style lang="less" scoped>
-@import '../theme-chalk/var.less';
-// .@{--prefixcls}__input__block {
-// }
+@import "../theme-chalk/var.less";
 
 .@{--prefixcls}__input {
     border: none;
     border: 0;
     border-radius: 4px;
     padding: 10px 12px;
-    width: 100%;
     color: @--text-input;
     outline: none;
     box-shadow: 0 1px 3px rgba(50, 50, 93, 0.15), 0 1px 0 rgba(0, 0, 0, 0.02);
     transition: box-shadow 0.15s ease;
 }
-
 </style>
